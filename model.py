f2e56258c1563f8bc271085ab31e7bef420b5c77
Michael Aleksa <mtaleksa1@gmail.com>
2020-03-26 00:23:07 -0400

    build model in __init__

--- model.py
+++ model.py
@@ -16,52 +16,47 @@ class MusicModel:
                  rnn_size: int,
                  layers: int,
                  dropout_rate: float,
-                 ckpt_dir: str = './training_checkpoints',
-                 ckpt_prefix: str = 'ckpt_{epoch'):
+                 ckpt_dir: str = './training_checkpoints'):
         """
-        Initialize parameters for model.
+        Initialize parameters, build and compile model.
         :param vocab_size: the number of classes for the model to learn/predict
         :param embed_dims: embedding layer dimensions
         :param rnn_size: lstm layer dimensions
         :param layers: number of (lstm, dropout, batch_normalization) layers
         :param dropout_rate: lstm-layer dropout rate
+        :param ckpt_dir: directory to save checkpoints
         """
-        # initialize parameters
         self.vocab_size = vocab_size
         self.embed_dims = embed_dims
         self.rnn_size = rnn_size
         self.layers = layers
         self.dropout_rate = dropout_rate
-        self.ckpt_path = Path(ckpt_dir) / Path(ckpt_prefix)
+        self.ckpt_path = Path(ckpt_dir) / 'ckpt_{epoch}'
         self.log_dir = Path('/logs') / Path(strftime("%Y-%m-%d-%H%M", localtime()))
-        self.model = None
-        self.callbacks = list()
-
-    def build_model(self):
-        """
-        Build and compile model.
-        """
-        self.model = keras.models.Sequential()
-        self.model.add(keras.layers.Embedding(self.vocab_size, self.embed_dims, batch_input_shape=[None, None]))
-        for _ in range(self.layers - 1):
-            self.model.add(keras.layers.LSTM(self.rnn_size, return_sequences=True))
-            self.model.add(keras.layers.Dropout(self.dropout_rate))
-            self.model.add(keras.layers.BatchNormalization())
-        self.model.add(keras.layers.LSTM(self.rnn_size))
-        self.model.add(keras.layers.Dropout(self.dropout_rate))
-        self.model.add(keras.layers.BatchNormalization())
-        self.model.add(keras.layers.Dense(units=self.vocab_size, activation='softmax'))
-
-        # compile model
+        ckpt_callback = tf.keras.callbacks.ModelCheckpoint(filepath=self.ckpt_path, save_weights_only=True)
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=self.log_dir)
+        self.callbacks = [ckpt_callback, tensorboard_callback]
+        self.model = self.__build_model()
         # TODO: learning rate scheduling
         self.model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-        # callbacks
-        ckpt_callback = tf.keras.callbacks.ModelCheckpoint(filepath=ckpt_path, save_weights_only=True)
-        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
-        self.callbacks = [ckpt_callback, tensorboard_callback]
+    def __build_model(self):
+        """
+        Build and compile model.
+        """
+        model = keras.models.Sequential()
+        model.add(keras.layers.Embedding(self.vocab_size, self.embed_dims, batch_input_shape=[None, None]))
+        for _ in range(self.layers - 1):
+            model.add(keras.layers.LSTM(self.rnn_size, return_sequences=True))
+            model.add(keras.layers.Dropout(self.dropout_rate))
+            model.add(keras.layers.BatchNormalization())
+        model.add(keras.layers.LSTM(self.rnn_size))
+        model.add(keras.layers.Dropout(self.dropout_rate))
+        model.add(keras.layers.BatchNormalization())
+        model.add(keras.layers.Dense(units=self.vocab_size, activation='softmax'))
+        return model
 
-    def train(self, data, val_data, epochs: int, batch_size: int, verbose: int = 0):
+    def fit(self, data, val_data, epochs: int, batch_size: int, verbose: int = 0):
         """
         Train model on dataset.
         :param data: dataset of event sequences
