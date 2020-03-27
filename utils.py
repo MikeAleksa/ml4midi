2790db68f590195af4684e80226599bd6a29f53a
Michael Aleksa <mtaleksa1@gmail.com>
2020-03-27 00:58:58 -0400

    add utils and check_cuda_and_gpu

--- utils.py
+++ utils.py
@@ -1,3 +1,20 @@
 """
 A collection of utilities for tuning and using the model.
 """
+
+import tensorflow as tf
+
+
+def check_cuda_and_gpu() -> list:
+    warnings = []
+
+    if not tf.test.is_built_with_cuda():
+        warnings.append('WARNING: TensorFlow not built with CUDA support.')
+    if not tf.test.is_built_with_gpu_support():
+        warnings.append('WARNING: TensorFlow not built with GPU support.')
+    if not tf.config.list_physical_devices('GPU'):
+        warnings.append('WARNING: No GPU Available.')
+    if not warnings:
+        warnings.append('CUDA and GPU Available...')
+
+    return warnings
