5eb31832288dc4576b6697352ca8918c85c1d2ae
Michael Aleksa <mtaleksa1@gmail.com>
2020-03-28 21:52:22 -0400

    add multi-sequence test for normalize()

--- tests/test_sequence_utils.py
+++ tests/test_sequence_utils.py
@@ -51,6 +51,8 @@ class TestSequenceUtils(TestCase):
         """
         Check normalization function.
         """
-        sequence = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
-        normalized = [[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]]
-        self.assertListEqual(normalized, sequence_utils.normalize(sequence, 11))
+        sequences = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    [3, 4, 5]]
+        normalized = [[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+                      [0.3, 0.4, 0.5]]
+        self.assertListEqual(normalized, sequence_utils.normalize(sequences, 11))
