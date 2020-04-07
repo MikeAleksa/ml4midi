2532c7decea97a7c48c4205df4416bdf6a12d64f
Michael Aleksa <mtaleksa1@gmail.com>
2020-04-07 16:02:21 -0400

    refactor MusicModel to inherit from keras.Sequential

--- model.py
+++ model.py
@@ -5,30 +5,26 @@ import tensorflow as tf
 import tensorflow.keras as keras
 
 
-class MusicModel:
+class MusicModel(keras.Sequential):
     """
     LSTM-based model for music generation.
     """
 
-    @property
-    def summary(self):
-        return self.model.summary
-
     def __init__(self,
                  n_classes: int = 356,
                  embed_dims: int = 128,
-                 rnn_size: int = 256,
+                 rnn_size: int = 512,
                  rnn_layers: int = 3,
-                 dense_size: int = 256,
+                 dense_size: int = 512,
                  dense_layers: int = 2,
                  dropout_rate: float = 0.1,
                  batch_norm: bool = True,
-                 init_lr: float = None,
+                 init_lr: float = 0.0003,
                  dense_activation: str = 'relu',
                  ckpt_dir: str = './training_checkpoints',
                  log_dir: str = './logs'):
         """
-        Initialize parameters, build and compile model.
+        Initialize parameters and build/compile model.
         :param n_classes: the number of classes for the model to learn/predict
         :param embed_dims: embedding layer dimensions
         :param rnn_size: lstm layer units
@@ -42,6 +38,7 @@ class MusicModel:
         :param ckpt_dir: directory to save checkpoints
         :param log_dir: directory to save tensorboard logs
         """
+        super().__init__()
         self.n_classes = n_classes
         self.embed_dims = embed_dims
         self.rnn_size = rnn_size
@@ -51,127 +48,133 @@ class MusicModel:
         self.dense_activation = dense_activation
         self.dropout_rate = dropout_rate
         self.batch_norm = batch_norm
+        self.init_lr = init_lr
         self.ckpt_path = str(Path(ckpt_dir) / 'ckpt_{epoch}')
         self.log_dir = log_dir
-        self.callbacks = self.__define_callbacks()
-        self.model = self.__build_model()
-        self.history = None
+        self.__build_model()
 
         def loss(labels, logits):
             return keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
         optimizer = keras.optimizers.Adam(learning_rate=init_lr, clipvalue=5.0)
 
-        self.model.compile(loss=loss, optimizer=optimizer, metrics=['sparse_categorical_accuracy'])
+        self.default_callbacks = self.__define_callbacks()
+        self.compile(loss=loss, optimizer=optimizer, metrics=['sparse_categorical_accuracy'])
 
     def __define_callbacks(self) -> list:
+        """
+        Define the default callbacks for MusicModel.
+        :return: a list of callbacks
+        """
         ckpt_callback = keras.callbacks.ModelCheckpoint(filepath=self.ckpt_path,
                                                         save_weights_only=True)
-        
+
         tensorboard_callback = keras.callbacks.TensorBoard(log_dir=self.log_dir,
                                                            histogram_freq=1,
                                                            profile_batch='2,5')
-        
+
         reduce_lr_callback = keras.callbacks.ReduceLROnPlateau(monitor='val_loss',
                                                                factor=0.3,
                                                                patience=2,
                                                                min_lr=0.000001,
                                                                cooldown=5,
                                                                verbose=1)
-        
+
         return [ckpt_callback,
                 reduce_lr_callback,
                 tensorboard_callback]
 
-    def __build_model(self) -> keras.Sequential:
+    def __build_model(self):
         """
-        Build and compile model.
-        :return: a keras sequential model
+        Build model using hyperparameters of MusicModel.
         """
-        model = keras.Sequential()
-
         # with embedding layer
         if self.embed_dims:
-            model.add(keras.layers.Embedding(self.n_classes, self.embed_dims, batch_input_shape=[None, None]))       
+            self.add(keras.layers.Embedding(self.n_classes, self.embed_dims, batch_input_shape=[None, None]))
             # lstm layers
             for _ in range(self.rnn_layers - 1):
-                model.add(keras.layers.LSTM(self.rnn_size, return_sequences=True))
+                self.add(keras.layers.LSTM(self.rnn_size, return_sequences=True))
                 if self.dropout_rate:
-                    model.add(keras.layers.Dropout(self.dropout_rate))
+                    self.add(keras.layers.Dropout(self.dropout_rate))
                 if self.batch_norm:
-                    model.add(keras.layers.BatchNormalization())
-            model.add(keras.layers.LSTM(self.rnn_size))
+                    self.add(keras.layers.BatchNormalization())
+            self.add(keras.layers.LSTM(self.rnn_size))
             if self.dropout_rate:
-                model.add(keras.layers.Dropout(self.dropout_rate))
+                self.add(keras.layers.Dropout(self.dropout_rate))
             if self.batch_norm:
-                model.add(keras.layers.BatchNormalization())
-                
+                self.add(keras.layers.BatchNormalization())
+
         # without embedding layer
         else:
-            model.add(keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-1), input_shape=[None]))
-            model.add(keras.layers.LSTM(self.rnn_size, return_sequences=True, input_shape=(None, 1)))
+            self.add(keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-1), input_shape=[None]))
+            self.add(keras.layers.LSTM(self.rnn_size, return_sequences=True, input_shape=(None, 1)))
             if self.dropout_rate:
-                model.add(keras.layers.Dropout(self.dropout_rate))
+                self.add(keras.layers.Dropout(self.dropout_rate))
             if self.batch_norm:
-                model.add(keras.layers.BatchNormalization())
+                self.add(keras.layers.BatchNormalization())
             for _ in range(self.rnn_layers - 2):
-                model.add(keras.layers.LSTM(self.rnn_size, return_sequences=True))
+                self.add(keras.layers.LSTM(self.rnn_size, return_sequences=True))
                 if self.dropout_rate:
-                    model.add(keras.layers.Dropout(self.dropout_rate))
+                    self.add(keras.layers.Dropout(self.dropout_rate))
                 if self.batch_norm:
-                    model.add(keras.layers.BatchNormalization())
-            model.add(keras.layers.LSTM(self.rnn_size))
+                    self.add(keras.layers.BatchNormalization())
+            self.add(keras.layers.LSTM(self.rnn_size))
             if self.dropout_rate:
-                model.add(keras.layers.Dropout(self.dropout_rate))
+                self.add(keras.layers.Dropout(self.dropout_rate))
             if self.batch_norm:
-                model.add(keras.layers.BatchNormalization())
+                self.add(keras.layers.BatchNormalization())
 
         # dense layers
         for _ in range(self.dense_layers - 1):
-            model.add(keras.layers.Dense(units=self.dense_size, activation=self.dense_activation))
+            self.add(keras.layers.Dense(units=self.dense_size, activation=self.dense_activation))
             if self.dropout_rate:
-                model.add(keras.layers.Dropout(self.dropout_rate))
+                self.add(keras.layers.Dropout(self.dropout_rate))
             if self.batch_norm:
-                model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dense(units=self.n_classes))
-
-        return model
+                self.add(keras.layers.BatchNormalization())
+        self.add(keras.layers.Dense(units=self.n_classes))
 
     def fit(self,
-            data,
-            val_data=None,
-            epochs: int = 1,
-            batch_size: int = None,
-            verbose: int = 0,
-            initial_epoch: int = 0) -> keras.callbacks.History:
+            x=None,
+            y=None,
+            batch_size=None,
+            epochs=1,
+            verbose=1,
+            callbacks=None,
+            validation_split=0.,
+            validation_data=None,
+            shuffle=True,
+            class_weight=None,
+            sample_weight=None,
+            initial_epoch=0,
+            steps_per_epoch=None,
+            validation_steps=None,
+            validation_freq=1,
+            max_queue_size=10,
+            workers=1,
+            use_multiprocessing=False,
+            **kwargs):
         """
-        Train model on dataset.
-        :param data: a dataset of event sequences - can be a tf.data.Dataset or tuple of (examples, labels)
-        :param val_data: a dataset of validation data
-        :param epochs: the number of epochs to train
-        :param batch_size: the size of batch to train on (not used with tf.data.Dataset)
-        :param verbose: verbosity
-        :param initial_epoch: epoch to start training at
-        :return: an object containing data about
+        Train the model for a fixed number of epochs, using keras.Sequential.fit() with default MusicModel callbacks.
         """
-        if not isinstance(data, tf.data.Dataset):
-            x, y = data
-            self.history = self.model.fit(x,
-                                          y,
-                                          epochs=epochs,
-                                          batch_size=batch_size,
-                                          validation_data=val_data,
-                                          verbose=verbose,
-                                          callbacks=self.callbacks,
-                                          initial_epoch=initial_epoch)
-        else:
-            self.history = self.model.fit(data,
-                                          epochs=epochs,
-                                          validation_data=val_data,
-                                          verbose=verbose,
-                                          callbacks=self.callbacks,
-                                          initial_epoch=initial_epoch)
-        return self.history
+        super().fit(x=None,
+                    y=None,
+                    batch_size=None,
+                    epochs=1,
+                    verbose=1,
+                    callbacks=self.default_callbacks + callbacks,
+                    validation_split=0.,
+                    validation_data=None,
+                    shuffle=True,
+                    class_weight=None,
+                    sample_weight=None,
+                    initial_epoch=0,
+                    steps_per_epoch=None,
+                    validation_steps=None,
+                    validation_freq=1,
+                    max_queue_size=10,
+                    workers=1,
+                    use_multiprocessing=False,
+                    **kwargs)
 
     def load_checkpoint(self, path: str, use_latest: bool = False):
         """
@@ -184,7 +187,7 @@ class MusicModel:
         else:
             ckpt = path
         try:
-            self.model.load_weights(ckpt)
+            self.load_weights(ckpt)
         except Exception as e:
             print(e)
 
@@ -192,7 +195,7 @@ class MusicModel:
         # TODO: implement finetune
         pass
 
-    def generate_sequence(self, length: int, seed_sequence: list, history: int = 128):
+    def generate_sequence(self, length: int, seed_sequence: list, history: int = 100):
         """
         Generate an event sequence using a given event sequence as a seed.
         :param length: the number of events to generate
@@ -205,7 +208,7 @@ class MusicModel:
         generated_sequence += seed_sequence
         for _ in range(length):
             sequence_history = np.array([generated_sequence[-history:]])
-            next_sample_logits = self.model(sequence_history)
+            next_sample_logits = self(sequence_history)
             next_sample = tf.random.categorical(next_sample_logits, 1)
             generated_sequence.append(int(next_sample))
         return generated_sequence[len(seed_sequence):]
